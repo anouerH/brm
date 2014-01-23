9f24c280edd33cfec417383aa742e92d2a606401
anouerH <anouermail@gmail.com>
2014-01-23 11:32:31 +0100

    add securtiy admin

--- src/Star/UserBundle/Controller/SecurityController.php
+++ src/Star/UserBundle/Controller/SecurityController.php
@@ -10,14 +10,24 @@ class SecurityController extends BaseController
 {
 
     public function renderLogin(array $data) {
-        
+    
         $requestAttributes = $this->container->get('request')->attributes;
         if ($requestAttributes->get('_route') == 'admin_login') {
-            $template = sprintf('StarUserBundle:Security:login.html.twig');
+           
+            //$template = sprintf('StarUserBundle:Security:login.html.twig');
+            $template = sprintf('StarUserBundle:Security:login-admin.html.%s', $this->container->getParameter('fos_user.template.engine'));
+            return $this->container->get('templating')->renderResponse($template, $data);
         } else {
-            $template = sprintf('FOSUserBundle:Security:login.html.twig');
+           
+            //$template = sprintf('FOSUserBundle:Security:login.html.%s', $this->container->getParameter('fos_user.template.engine'));
+            return  parent::renderLogin($data);
         }
-        return $this->container->get('templating')->renderResponse($template, $data);
+        
+        
     }
+
+
+
+   
     
 }
